e9bef7004107ea7d4fb98bc605585577abc08155
sihan sun <haniyya2@gmail.com>
2021-04-02 20:58:05 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React , {useState, useEffect} from "react"
+import Character from "./components/Character"
+import axios from "axios"
+import styled from "styled-components"
 import './App.css';
+import {Container, Row, Col} from 'reactstrap'
 
+
+const StyledHeader = styled.div`
+  h1{
+    color: white;
+  }
+  button{
+    background-color: black;
+    color: white;
+    border: 2px solid green;
+    text-align: center;
+    display: block;
+    margin:auto;
+    font-size: 100px
+    position: relative;
+  
+  }
+`
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -9,10 +30,38 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [character, setCharacter] = useState([])
+  const [currentCharacter, setCurrentCharacter] = useState(null)
+
+  const openDetails = item => {
+      setCurrentCharacter(item)
+  }
+  const closeDetails = () => {
+      setCurrentCharacter(null)
+  }
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+        .then(res => {
+            console.log(res.data)
+            setCharacter(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+  }, [])
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyledHeader className="App">
+      <Container>
+        <h1>REACT WARS </h1>
+          <Row>
+          {character.map ((item, index)=> (
+            <button key = {index} onClick = {() => openDetails(item)}>{item.name}</button>
+          ))}
+          {currentCharacter && < Character currentCharacter = {currentCharacter}  close = {closeDetails} character = {character}/>}
+          </Row>
+      </Container>
+    </StyledHeader>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,21 @@
 // Write your Character component here
+import React from "react"
+import styled from "styled-components"
+
+const StyleDetail = styled.div`
+    color: white;
+`
+export default function Character({currentCharacter ,close, character}){
+
+    return (
+        <StyleDetail className = "details">
+            <p> Gender: {currentCharacter.gender}</p>
+            <p> Height: {currentCharacter.height}</p>
+            <p> Mass: {currentCharacter.mass}</p>
+            <p> Birth Year: {currentCharacter.birth_year}</p>
+            <p> Eye Color: {currentCharacter.eye_color}</p>
+            <p> Hair Color: {currentCharacter.hair_color}</p>
+            <p> Skin Color: {currentCharacter.skin_color}</p>
+        </StyleDetail>
+    )
+}
